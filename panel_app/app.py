be1d380cf24bf04ebeff9d2d959e440280466117
anton <anton.biryukov@pason.com>
2020-03-27 15:46:13 -0600

    feat: adding destinations with cache

--- panel_app/app.py
+++ panel_app/app.py
@@ -25,23 +25,58 @@ def default_altair(lines=False):
     return ch
 
 
+def create_destination_inputs(n=2, prev_destinations=None):
+    if prev_destinations is None:
+        wlist = []
+        for i in range(n):
+            name_widget = f'Destination {i + 1}'
+            widget = pn.widgets.TextInput(name=name_widget, value='')
+            wlist.append(widget)
+
+    else:
+        wlist = prev_destinations
+        n_old = len(prev_destinations)
+        if n > n_old:
+            for i in range(n_old + 1, n):
+                name_widget = f'Destination {i + 1}'
+                widget = pn.widgets.TextInput(name=name_widget, value='')
+                wlist.append(widget)
+        else:
+            print(f'Nold: {n_old} , new: {n}')
+            wlist = wlist[0:n]
+
+    widget_all = pn.Column(*wlist)
+    return widget_all, wlist
+
+
 class ReactiveForecastDashboard(param.Parameterized):
     title = pn.pane.Markdown('# Booze Cruise YYC')
     # Add a widget that picks the environment and bucket
-    number_dest = param.Integer(5,label='Select number of destinations', bounds=(0, 15))
+    number_dest = param.Integer(5, label='Select number of destinations', bounds=(0, 15))
     progress_bar = pnw.misc.Progress(active=False, bar_color='light', value=None, width_policy='max',
                                      sizing_mode='stretch_width')
+
     date_custom_map = {}
     get_locations_action = pnw.Button(name="Get Locations", button_type='primary')
     get_best_route_action = pnw.Button(name="Get Best Route", button_type='default')
+    destinations_pane, destinations_wlist = create_destination_inputs(n=5, prev_destinations=None)
 
     all_dates_forecast = default_altair()
     default_plot = pn.Pane(default_altair())
 
     # Create a reusable Paginator
-    #@param.depends('bucket_string', 'env_string')
+    # @param.depends('bucket_string', 'env_string')
     def get_listing_s3(self):
         pass
+
+    @param.depends('number_dest', watch=True)
+    def change_destinations_number(self):
+        print(self.number_dest)
+        new_destinations = create_destination_inputs(n=self.number_dest,
+                                                     prev_destinations=self.destinations_wlist)
+        self.destinations_pane, self.destinations_wlist = new_destinations[0], new_destinations[1]
+        print(self.destinations_wlist)
+        return self.destinations_pane
 
     def panel(self):
         self.get_locations_action.on_click(lambda x: print(x))
@@ -50,7 +85,7 @@ class ReactiveForecastDashboard(param.Parameterized):
         widgets_ = self.param
         buttons_ = pn.Column(self.get_locations_action, self.get_best_route_action)
         progress_bar = pn.Pane(self.progress_bar, sizing_mode='stretch_width', width_policy='max')
-        result = pn.Row(pn.Column(self.title, widgets_,buttons_,
+        result = pn.Row(pn.Column(self.title, widgets_, buttons_, self.change_destinations_number,
                                   progress_bar),
                         pn.Column(self.default_plot,
                                   sizing_mode='stretch_width', width_policy='max'),
