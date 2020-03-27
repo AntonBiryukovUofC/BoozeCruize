ac3383ddd02c8044cae1cb5520bbf7e72b1e094d
Mike Birdgeneau <mike.birdgeneau@gmail.com>
2020-03-27 08:58:33 -0600

    Fix config ownership.

--- nominatim/start.sh
+++ nominatim/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+chown -R root /etc/postgresql
+
 stopServices() {
         service apache2 stop
         service postgresql stop
--- nominatim/startpostgres.sh
+++ nominatim/startpostgres.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+chown -R /etc/postgresql
+
 sudo -u postgres psql postgres -tAc "GRANT usage ON SCHEMA public TO 'www-data';"
 sudo -u postgres psql postgres -tAc "GRANT SELECT ON ALL TABLES IN SCHEMA public TO 'www-data';"
 
