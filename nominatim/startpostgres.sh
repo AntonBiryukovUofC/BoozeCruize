9c89dcd1a68f834c9e1b898531e37cbe2c9be9f5
Mike Birdgeneau <mike.birdgeneau@gmail.com>
2020-03-27 12:00:08 -0600

    Tweak a few things. Still not working.

--- nominatim/startpostgres.sh
+++ nominatim/startpostgres.sh
@@ -3,7 +3,7 @@
 chown -R postgres:postgres /etc/postgresql
 
 service postgresql start
-#sudo -u postgres psql postgres -tAc "GRANT usage ON SCHEMA public TO 'www-data';"
-#sudo -u postgres psql postgres -tAc "GRANT SELECT ON ALL TABLES IN SCHEMA public TO 'www-data';"
+#sudo -u postgres psql postgres -tAc 'GRANT usage ON SCHEMA public TO "www-data";'
+#sudo -u postgres psql postgres -tAc 'GRANT SELECT ON ALL TABLES IN SCHEMA public TO "www-data";'
 
 tail -f /var/log/postgresql/postgresql-11-main.log
