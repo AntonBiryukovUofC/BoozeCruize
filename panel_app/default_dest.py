54d55d28fc9be9ba32b7cae7d4439cabdb3fd123
anton <anton.biryukov@pason.com>
2020-03-29 00:48:14 -0600

    feat: added geocoding functionality using HERE

---
+++ panel_app/default_dest.py
@@ -0,0 +1,10 @@
+DEFAULT_DEST = [
+    "1122 3 St SE",
+    "303-1706 11 ave sw",
+    "445 Marsh Rd NE",
+    "504 Berkley Cres NW",
+    "880 Center Ave NE",
+    "111 25 Ave SW",
+    "7016 34 Ave NW",
+    "2313 15A st SE",
+]
