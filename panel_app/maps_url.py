c7d29e967574e96af687b20ef15395eec7c7d33c
Mike Birdgeneau <mike.birdgeneau@gmail.com>
2020-03-31 22:38:27 -0600

    Just some code formatting.

--- panel_app/maps_url.py
+++ panel_app/maps_url.py
@@ -8,26 +8,24 @@ https://developers.google.com/maps/documentation/urls/guide#directions-action
 
 import requests
 
-def build_map_url(origin,waypoints,destination = None,travelmode='driving'):
 
+def build_map_url(origin, waypoints, destination=None, travelmode='driving'):
     if destination is None:
         destination = origin
-    
+
     payload = {
-        'origin':origin,
-        'waypoints':'|'.join(waypoints),
-        'destination':destination,
-        'travelmode':travelmode,
-        'dir_action':'navigate'
+        'origin': origin,
+        'waypoints': '|'.join(waypoints),
+        'destination': destination,
+        'travelmode': travelmode,
+        'dir_action': 'navigate'
     }
 
-    r = requests.get('https://www.google.com/maps/dir/?api=1',params=payload)
+    r = requests.get('https://www.google.com/maps/dir/?api=1', params=payload)
 
-    return(r)
+    return (r)
 
-
-
-#print(
+# print(
 #     build_map_url(
 #         origin="4407 1 Street Calgary, Alberta",
 #         waypoints=[
@@ -37,4 +35,3 @@ def build_map_url(origin,waypoints,destination = None,travelmode='driving'):
 #     ).url
 # )
 ## Response: https://www.google.com/maps/dir/?api=1&origin=4407+1+Street+Calgary,+Alberta&waypoints=500+Centre+Street+SE+Calgary,+Alberta|88+Canada+Olympic+Rd+SW,+Calgary,+AB&destination=4407+1+Street+Calgary,+Alberta&travelmode=driving&dir_action=navigate
-
