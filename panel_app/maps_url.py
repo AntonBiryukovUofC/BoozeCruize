4e401946507890cf2124b910a46debd98a9cc122
Mike Birdgeneau <mike.birdgeneau@gmail.com>
2020-04-03 20:28:41 -0600

    Testing fetch of URL with geckodriver.

--- panel_app/maps_url.py
+++ panel_app/maps_url.py
@@ -7,7 +7,11 @@ https://developers.google.com/maps/documentation/urls/guide#directions-action
 """
 
 import requests
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
 
+import time
 
 def build_map_url(origin, waypoints, destination=None, travelmode='driving'):
     if destination is None:
@@ -25,37 +29,26 @@ def build_map_url(origin, waypoints, destination=None, travelmode='driving'):
 
     return (r)
 
+def construct_gmaps_urls(latlongs_list,waypoints_batch_size=0):
+    # Set-up to get url with 10 points:
+    if len(latlongs_list)>=10:
+        r = build_map_url(origin=latlongs_list[0], destination=latlongs_list[10], waypoints=latlongs_list[1:9])
+    else:
+        r = build_map_url(origin=latlongs_list[0], destination=latlongs_list[-1], waypoints=latlongs_list[1:-1])
 
-def construct_gmaps_urls(latlongs_list, waypoints_batch_size=2):
-    sub_latlong_list = []
-    batches = []
-    urls = []
-    i = 0
-    for ll in latlongs_list:
-        # if (i > 4) or (i==len(latlongs_list)):
-        # +2 is for origin + destination
-        if len(sub_latlong_list) + 1 <= (waypoints_batch_size + 2):
-            sub_latlong_list.append(ll)
-        else:
-            end_point = sub_latlong_list[-1]
-            batches.append(sub_latlong_list)
-            sub_latlong_list = [end_point, ll]
-        i += 1
-    # Exited for loop with the last element
-    if len(sub_latlong_list) > 1:
-        batches.append(sub_latlong_list)
-    # Create URLs:
-    for batch in batches:
-        wp_list = batch
-        #wp_list = [f'{x[0]},{x[1]}' for x in batch]
-        if len(wp_list) >=3 :
-            r = build_map_url(origin=wp_list[0], destination=wp_list[-1], waypoints=wp_list[1:-1])
-        else:
-            r = build_map_url(origin=wp_list[0], destination=wp_list[-1], waypoints=[])
-        urls.append(r.url)
+    options = Options()
+    options.headless = False
+    driver = webdriver.Firefox(options=options)
+
+    driver.get(r.url)
+    WebDriverWait(driver, 10).until(lambda driver: driver.execute_script('return document.readyState') == 'complete')
+    url = driver.current_url
+    print(url)
+
+    batches = [0]
+    urls = [url]
 
     return batches, urls
-    # Now we have batches ready, we need to construct urls
 
 
 def rearrange_waypoints(response_json):
